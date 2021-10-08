60c3dbc091006c6f6dd15041f811d2b3a163c77d
robustTechie <captain.shank75@gmail.com>
2021-10-08 13:15:58 +0530

    add env

--- bot.py
+++ bot.py
@@ -3,11 +3,14 @@ from discord.ext import commands
 from discord.utils import get
 import requests
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 intents = discord.Intents.all()
 intents.members = True
 bot=commands.Bot(command_prefix='$',intents=intents)
-GUILD= {"Saurav's-Gaming-Room"}  #Your server name
+GUILD= {os.getenv('CHANNEL_NAME')}  #Your server name
 
 #bot events
 @bot.event
